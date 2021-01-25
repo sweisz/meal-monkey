0dd86a66505905fb4cfe1fe1ec5fa65dcfe067c7
sweisz <dev@example.com>
2021-01-25 15:42:04 +0100

    Changed Buttonstyling

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,11 +1,11 @@
 import "./button.css";
-import buttonText from "./button-text.html";
-import buttonLogo from "./button-logo.html";
+import buttonfilled from "./button-filled.html";
+import buttonborder from "./button-border.html";
 
 export default {
   title: "Components/Button",
   parameters: { layout: "centered" },
 };
 
-export const withText = () => buttonText;
-export const withLogo = () => buttonLogo;
+export const filled = () => buttonfilled;
+export const border = () => buttonborder;
