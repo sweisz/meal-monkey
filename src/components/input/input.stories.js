1728c1f71057e3574c478abf3a2cdcee279fcdf9
sweisz <dev@example.com>
2021-01-25 16:44:32 +0100

    Styling a input-field.

---
+++ src/components/input/input.stories.js
@@ -0,0 +1,9 @@
+import "./input.css";
+import inputemail from "./input-email.html";
+
+export default {
+  title: "Components/Input",
+  parameters: { layout: "centered" },
+};
+
+export const email = () => inputemail;
