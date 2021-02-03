89036cad3c50df8a1db0bdc64e91f3f74e2d40be
sweisz <dev@steffenweisz.de>
2021-02-03 12:05:15 +0100

    ♻️ password container function

--- src/components/form/verify-form.js
+++ src/components/form/verify-form.js
@@ -22,19 +22,49 @@ export function createVerifyForm() {
   infoText.innerText =
     "Please check your mobile number ... Continue to reset your password.";
 
-  form.append(title, infoText);
-
-  for (let i = 0; i < 4; i++) {
+  // this Function creates a input
+  function createOTPInputElement() {
     const input = document.createElement("input");
-    input.setAttribute("type", "password");
-    input.setAttribute("placeholder", "*");
-    input.classList.add("otpPass");
-    form.append(input);
+    input.type = "password";
+    input.placeholder = "*";
+    input.className = "input";
+    return input;
   }
+
+  // Create variables via function
+  const otpOne = createOTPInputElement();
+  const otpTwo = createOTPInputElement();
+  const otpThree = createOTPInputElement();
+  const otpFour = createOTPInputElement();
+
+  // Create the container DIV for OTP Password or PIN
+  const passwordContainer = document.createElement("div");
+  passwordContainer.className = "form__otp";
+  passwordContainer.append(otpOne, otpTwo, otpThree, otpFour);
+
+  form.append(title, passwordContainer, infoText);
+
+  // for (let i = 0; i < 4; i++) {
+  //   const input = document.createElement("input");
+  //   input.setAttribute("type", "password");
+  //   input.setAttribute("placeholder", "*");
+  //   input.classList.add("otpPass");
+  //   form.append(input);
+  // }
+
   form.append(button);
 
-  const infoText = document.createElement("p");
-  infoText.innerText = "Did not receive? Click here.";
+  // Hint +Link under the button
+  const hint = document.createElement("p");
+  hint.innerText = "Didn't Receive?";
+  hint.className = "form__hint";
+
+  const resendLink = document.createElement("a");
+  resendLink.innerText = "Click Here";
+  resendLink.href = "#";
+
+  form.append(hint);
+  hint.append(resendLink);
 
   return form;
 }
